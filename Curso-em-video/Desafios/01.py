5c9afbb796c34f3ebf3f7357f925b44d0190ac68
Michael-Brendo <Michael-Brendo@users.noreply.github.com>
2020-04-13 20:25:41 -0300

    Desafios 01 a 03

---
+++ Curso-em-video/Desafios/01.py
@@ -0,0 +1,2 @@
+name = input('type your name: ')
+print ('Wellcome', name)
---
+++ Curso-em-video/Desafios/02.py
@@ -0,0 +1,4 @@
+Day = input("Day: ")
+Month = input("Month: ")
+Year = input("Year: ")
+print ("your birth day is", Month + "/" + Day + "/" + Year + ",", "right?")
