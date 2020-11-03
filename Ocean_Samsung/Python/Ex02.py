354db6a36468d0566d0f14c592c4d2b9ef759a93
Michael <maicow259@gmail.com>
2020-11-03 16:55:17 -0300

    Create Ex02.py

---
+++ Ocean_Samsung/Python/Ex02.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome ").title()
+idade = int(input("Digite sua idade "))
+genero = input("Digite seu genero ").title()
+
+print(f"Olá {nome}, você possui {idade} anos de idade e é do gênero {genero}")
+
+print(f"Ja pensou no que você fará no seu aniversario de {idade + 1} anos?")
