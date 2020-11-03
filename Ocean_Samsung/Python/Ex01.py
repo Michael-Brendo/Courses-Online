be3e7d137f252319816deeeda08ebc43b9f341bd
Michael <maicow259@gmail.com>
2020-11-03 16:42:13 -0300

    Create Ex01.py

---
+++ Ocean_Samsung/Python/Ex01.py
@@ -0,0 +1,7 @@
+valor_refeicao = 42.54
+taxa_servico = 10
+
+total_conta = valor_refeicao + valor_refeicao * 10 / 100
+
+print(f"Sua refeição custou R$ {valor_refeicao}, mais a taxa de serviço de {taxa_servico}%.")
+print(f"O valor total ficou de R$ {total_conta:.2f}.")
