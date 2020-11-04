ceb5f2033fb76d550d8105db2cdc5e44e3523c8a
Michael <maicow259@gmail.com>
2020-11-04 10:43:41 -0300

    Ex02

--- Ocean_Samsung/Python/Fábrica de Sinapse/Ex01.py
+++ Ocean_Samsung/Python/Fábrica de Sinapse/Ex01.py
@@ -1,6 +1,7 @@
 a = 7
 b = 12
 resultado = a + b
+
 print(a + b)
 print(a - b)
 print(a * b)
---
+++ Ocean_Samsung/Python/Fábrica de Sinapse/Ex02.py
@@ -0,0 +1,5 @@
+total_conta = 82.57
+valor_pago = 100
+troco = valor_pago - total_conta
+
+print(f"O total da conta foi de R$ {total_conta:.2f} você pagou R$ {valor_pago:.2f} e o seu troco foi de R$ {troco:.2f}")
